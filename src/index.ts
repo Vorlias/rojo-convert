8464530699e6e87691e7ec8aed7c4d846de199b8
Jonathan Holmes <pcf1235@aut.ac.nz>
2019-02-11 04:40:13 +1300

    changes

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
+#!/usr/bin/env node
 import fs from "fs-extra";
+import path from "path";
+
+const rojoJSON = path.resolve(process.cwd(), "rojo.json");
+
+if (fs.existsSync(rojoJSON)) {
+} else {
+	console.error(`Failed to find ${rojoJSON}`);
+}
